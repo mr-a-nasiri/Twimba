87b92a3b836c0a8fe4f96073ec8c273f7cee00db
mr-a-nasiri <mrfinal21@gmail.com>
2022-11-03 20:04:18 +0330

    Add ability to remove tweets and replies, finish.

--- js/script.js
+++ js/script.js
@@ -1,30 +1,48 @@
 'use strict';
 
-// Import Data
+///////////////////////////////////////////////////////////
+/////----------- Import Data -----------/////
+///////////////////////////////////////////////////////////
 import { tweetsData } from '/js/data.js';
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
-// Elements & Input
-// Btn Elements
-// Values
-// Event listeners
+///////////////////////////////////////////////////////////
+/////----------- Event Listeners -----------/////
+///////////////////////////////////////////////////////////
 document.addEventListener('click', function (e) {
+  // Listen for like
   if (e.target.dataset.like) {
     handleLikeBtn(e.target.dataset.like);
-  } else if (e.target.dataset.retweet) {
+  }
+  // Listen for retweet
+  else if (e.target.dataset.retweet) {
     handleRetweetBtn(e.target.dataset.retweet);
-  } else if (e.target.dataset.reply) {
+  }
+  // Listen for show replies
+  else if (e.target.dataset.reply) {
     handleReplyBtn(e.target.dataset.reply);
-  } else if (e.target.dataset.replysend) {
+  }
+  // Listen for send reply
+  else if (e.target.dataset.replysend) {
     handleReplySendBtn(e.target.dataset.replysend);
-  } else if (e.target.id === 'tweet-btn') {
-    handTweedBtn();
+  }
+  // Listen for send tweet
+  else if (e.target.id === 'tweet-btn') {
+    handleTweetBtn();
+  }
+  // Listen for remove tweet
+  else if (e.target.dataset.remove) {
+    handleRemoveTweetBtn(e.target.dataset.remove);
+  }
+  // Listen for remove reply
+  else if (e.target.dataset.removereply) {
+    handleRemoveReplyBtn(e.target.dataset.removereply);
   }
 });
-// loadEventListeners();
-// LocalStorage Functions
-// Utility Functions
-// Functions
+
+///////////////////////////////////////////////////////////
+/////----------- Render Functions -----------/////
+///////////////////////////////////////////////////////////
 function render() {
   let tweetsHtml = '';
 
@@ -52,6 +70,11 @@ function render() {
                 <div>
 
                     <p class="handle">${tweet.handle}</p>
+                    ${
+                      tweet.isUser
+                        ? `<i class="fa-solid fa-circle-xmark remove-btn" id="remove-btn" data-remove="${tweet.uuid}"></i>`
+                        : ''
+                    }
                     <p class="tweet-text">${tweet.tweetText}</p>
 
                     <div class="tweet-details">
@@ -79,22 +102,27 @@ render();
 
 function renderReplies(tweet) {
   let repliesHtml = `
-  <textarea id="reply-input-${tweet.uuid}" class="reply-input"></textarea>
-  <button class="reply-btn" data-replysend="${tweet.uuid}">Send</button>
-  `;
+    <textarea id="reply-input-${tweet.uuid}" class="reply-input"></textarea>
+    <button class="reply-btn" data-replysend="${tweet.uuid}">Send</button>
+    `;
 
   tweet.replies.forEach(function (reply) {
     repliesHtml += `
-            <div class="tweet-reply">
-                <div class="tweet-inner">
-                    <img src="${reply.profilePic}" class="profile-pic">
-                        <div>
-                            <p class="handle">${reply.handle}</p>
-                            <p class="tweet-text">${reply.tweetText}</p>
-                        </div>
-                    </div>
-            </div>
-            `;
+              <div class="tweet-reply">
+                  <div class="tweet-inner">
+                      <img src="${reply.profilePic}" class="profile-pic">
+                          <div>
+                              <p class="handle">${reply.handle}</p>
+                              ${
+                                reply.isUser
+                                  ? `<i class="fa-solid fa-circle-xmark remove-btn" id="remove-btn" data-removereply="${reply.uuid}"></i>`
+                                  : ''
+                              }
+                              <p class="tweet-text">${reply.tweetText}</p>
+                          </div>
+                      </div>
+              </div>
+              `;
   });
 
   return repliesHtml;
@@ -102,10 +130,10 @@ function renderReplies(tweet) {
 
 function renderReplyBtn(tweet) {
   return `<span class="tweet-detail" id="replies-btn-${tweet.uuid}">
-    <i class="fa-regular fa-comment-dots"
-    data-reply="${tweet.uuid}"></i>
-    ${tweet.replies.length}
-    </span>`;
+      <i class="fa-regular fa-comment-dots"
+      data-reply="${tweet.uuid}"></i>
+      ${tweet.replies.length}
+      </span>`;
 }
 
 function renderLikeBtn(tweet) {
@@ -113,11 +141,11 @@ function renderLikeBtn(tweet) {
   if (tweet.isLiked) likedClass = 'liked';
 
   return `<span class="tweet-detail" id="like-btn-${tweet.uuid}">
-    <i class="fa-solid fa-heart ${likedClass}"
-    data-like="${tweet.uuid}"></i>
-    ${tweet.likes}
-    </span>
-    `;
+      <i class="fa-solid fa-heart ${likedClass}"
+      data-like="${tweet.uuid}"></i>
+      ${tweet.likes}
+      </span>
+      `;
 }
 
 function renderRetweetBtn(tweet) {
@@ -125,13 +153,79 @@ function renderRetweetBtn(tweet) {
   if (tweet.isRetweeted) retweetedClass = 'retweeted';
 
   return `<span class="tweet-detail" id="retweet-btn-${tweet.uuid}">
-    <i class="fa-solid fa-retweet ${retweetedClass}"
-    data-retweet="${tweet.uuid}"></i>
-    ${tweet.retweets}
-    </span>
-    `;
+      <i class="fa-solid fa-retweet ${retweetedClass}"
+      data-retweet="${tweet.uuid}"></i>
+      ${tweet.retweets}
+      </span>
+      `;
 }
 
+///////////////////////////////////////////////////////////
+/////----------- Handle Btn Functions -----------/////
+///////////////////////////////////////////////////////////
+
+// Add Tweet & Replies
+function handleReplySendBtn(uuid) {
+  const replyInput = document.getElementById(`reply-input-${uuid}`).value;
+  const targetTweet = getTargetTweet(uuid);
+
+  const reply = {
+    handle: `@Scrimba ✅`,
+    profilePic: `images/scrimbalogo.png`,
+    tweetText: replyInput,
+    isUser: true,
+    uuid: uuidv4(),
+  };
+
+  if (replyInput) targetTweet.replies.unshift(reply);
+
+  document.getElementById(`replies-btn-${uuid}`).innerHTML = renderReplyBtn(targetTweet);
+
+  document.getElementById(`replies-${uuid}`).innerHTML = renderReplies(targetTweet);
+}
+
+function handleTweetBtn() {
+  let tweetInput = document.getElementById(`tweet-input`);
+
+  const tweet = {
+    handle: `@Scrimba ✅`,
+    profilePic: `images/scrimbalogo.png`,
+    likes: 0,
+    retweets: 0,
+    tweetText: tweetInput.value,
+    replies: [],
+    isLiked: false,
+    isRetweeted: false,
+    isUser: true,
+    uuid: uuidv4(),
+  };
+
+  if (tweetInput.value) tweetsData.unshift(tweet);
+  render();
+
+  tweetInput.value = '';
+}
+
+// Remove Tweet & Replies
+function handleRemoveTweetBtn(uuid) {
+  const targetTweet = getTargetTweet(uuid);
+  const targetTweetIndex = tweetsData.indexOf(targetTweet);
+  tweetsData.splice(targetTweetIndex, 1);
+  render();
+}
+
+function handleRemoveReplyBtn(uuid) {
+  const targetArray = getTargetArray(uuid);
+
+  const [targetTweetReply, targetTweet] = targetArray;
+
+  const targetTweetReplyIndex = targetTweet.replies.indexOf(targetTweetReply);
+  targetTweet.replies.splice(targetTweetReplyIndex, 1);
+
+  document.getElementById(`replies-${targetTweet.uuid}`).innerHTML = renderReplies(targetTweet);
+}
+
+// Like, Retweet, Show replies
 function handleLikeBtn(uuid) {
   const targetTweet = getTargetTweet(uuid);
 
@@ -161,22 +255,9 @@ function handleReplyBtn(uuid) {
   document.getElementById(`replies-${uuid}`).classList.toggle('hidden');
 }
 
-function handleReplySendBtn(uuid) {
-  const replyInput = document.getElementById(`reply-input-${uuid}`).value;
-  const targetTweet = getTargetTweet(uuid);
-
-  const reply = {
-    handle: `@Scrimba ✅`,
-    profilePic: `images/scrimbalogo.png`,
-    tweetText: replyInput,
-  };
-
-  if (replyInput) targetTweet.replies.unshift(reply);
-
-  document.getElementById(`replies-btn-${uuid}`).innerHTML = renderReplyBtn(targetTweet);
-
-  document.getElementById(`replies-${uuid}`).innerHTML = renderReplies(targetTweet);
-}
+///////////////////////////////////////////////////////////
+/////----------- Utility Functions -----------/////
+///////////////////////////////////////////////////////////
 
 function getTargetTweet(uuid) {
   return tweetsData.filter(function (tweet) {
@@ -184,21 +265,15 @@ function getTargetTweet(uuid) {
   })[0];
 }
 
-function handTweedBtn() {
-  const tweetInput = document.getElementById(`tweet-input`).value;
-
-  const tweet = {
-    handle: `@Scrimba ✅`,
-    profilePic: `images/scrimbalogo.png`,
-    likes: 0,
-    retweets: 0,
-    tweetText: tweetInput,
-    replies: [],
-    isLiked: false,
-    isRetweeted: false,
-    uuid: uuidv4(),
-  };
-
-  if (tweetInput) tweetsData.unshift(tweet);
-  render();
+function getTargetArray(uuid) {
+  let targetReply = [];
+  for (let tweet of tweetsData) {
+    for (let reply of tweet.replies) {
+      if (reply.uuid === uuid) {
+        targetReply.push(reply);
+        targetReply.push(tweet);
+      }
+    }
+  }
+  return targetReply;
 }
